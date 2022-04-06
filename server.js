0d8b4b97e3d59928069ad2b4c7cc88a439c92650
Nusoft <robert.tomlin@eagleteleservices.biz>
2022-04-06 13:54:24 -0400

    improved queue

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const zmq = require("zeromq");
+const zmq = require("zeromq")
 
-const sock = new zmq.Push();
-run();
+const sock = new zmq.Push()
+
 async function run () {
 
     await sock.bind("tcp://127.0.0.1:7000");
-    console.log("Server is ready listening on port 7000");
+    console.log("Server is ready listening on port 7000")
     console.log("Press any key to start sending the jobs!")
     process.stdin.once("data", send); 
 }
@@ -13,10 +13,12 @@ async function run () {
 //sending the jobs to the workers
 async function send () {
 
-    console.log("About to send jobs!");
+    console.log("About to send jobs!")
     for (let i = 0 ;i < 100; i ++) {
-        await sock.send(`sending job ${i}`);
+        await sock.send(`sending job ${i}`)
         //wait 500ms
         await new Promise(resolve => setTimeout(resolve, 500))
     }
 }
+
+run()
