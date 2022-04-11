71c093bf1c6c7c716b44e86a9643089485980f7b
Nusoft <bobtomlin@gmail.com>
2022-04-11 09:18:27 -0400

    Modified Async

--- receiver.js
+++ receiver.js
@@ -2,13 +2,13 @@ const zmq = require("zeromq")
 const sock = new zmq.Pull()
 
 
-async function run() {
+const run = async () => {
 
     await sock.connect("tcp://127.0.0.1:7000")
     console.log("Connected to server.")
 
     for await (const msg of sock) {
-        console.log(`received job ${msg.toString()}`)
+        console.log(`received message ${msg.toString()}`)
     }
 } 
 
--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const zmq = require("zeromq")
 
 const sock = new zmq.Push()
 
-async function run () {
+const run = async () => {
 
-    await sock.bind("tcp://127.0.0.1:7000");
+    await sock.bind("tcp://127.0.0.1:7000")
     console.log("Server is ready listening on port 7000")
-    console.log("Press any key to start sending the jobs!")
+    console.log("Press any key to start sending the messages!")
     process.stdin.once("data", send); 
 }
 
-//sending the jobs to the workers
-async function send () {
+//sending the jobs to the receivers
+const send = async ()=>  {
 
-    console.log("About to send jobs!")
-    for (let i = 0 ;i < 100; i ++) {
+    console.log("Sending messages")
+    for (let i = 0 ;i < 1000; i ++) {
         await sock.send(`sending job ${i}`)
-        //wait 500ms
-        await new Promise(resolve => setTimeout(resolve, 500))
+        //wait 50ms
+        await new Promise(resolve => setTimeout(resolve, 50))
     }
+    console.log("Done sending")
 }
 
 run()
